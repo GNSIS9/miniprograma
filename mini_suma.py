899c92a65ac5520bb5003131bc53e6153585c920
GNSIS9 <jirinu91@gmail.com>
2023-06-13 20:24:11 +0200

    Cambio del primer numero de la suma

--- mini_suma.py
+++ mini_suma.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
-# Definir los números
-num1 = 5
-num2 = 3
+from pymongo import MongoClient
 
-# Sumar los dos números
-suma = num1 + num2
+class RepositorioPeliculas:
+    
+   
+    def __init__(self):
+        self.peliculas = []
+        self.cliente = MongoClient("mongodb://localhost:27017/") # Conexión a MongoDB
+        self.db = self.cliente["peliculas_db"]
+        self.coleccion = self.db["peliculas"]
+        
+    def actualizar_peliculas(self):
+        query = {"titulo": "Matrix"}  
+        documentos = self.coleccion.find(query)
 
-# Imprimir el resultado
-print("La suma es:", suma)
+        for documento in documentos:
+            documento["director"] = "Lana Wachowski"
+        self.coleccion.replace_one({"_id": documento["_id"]}, documento)
+
+        print("Las películas se han actualizado correctamente en la base de datos.")
+        self.cliente.close()
+        
+        x = RepositorioPeliculas()
+        x.actualizar_peliculas()
+      
+      
